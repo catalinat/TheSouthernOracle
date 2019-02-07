10ca0716f7c2046f52167d5ec51eee86c9c4812c
Doi90 <davidpw@student.unimelb.edu.au>
2019-02-07 11:24:25 +1100

    Answer to group 3

---
+++ Group_3.R
@@ -0,0 +1,8 @@
+## This thing all things devours:
+## Birds, beasts, trees, flowers;
+## Gnaws iron, bites steel;
+## Grinds hard stones to meal;
+## Slays king, ruins town,
+## And beats high mountain down.
+
+Answer <- "Time"
